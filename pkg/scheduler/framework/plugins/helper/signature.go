2f0bdb3eff4fbe17dd8a63ac804bcf120cc5ad48
Brandon Salmon <bsalmon@google.com>
2025-10-01 02:09:14 +0000

    Comments.

--- pkg/scheduler/framework/plugins/helper/signature.go
+++ pkg/scheduler/framework/plugins/helper/signature.go
@@ -6,14 +6,25 @@ import (
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 )
 
+// The given pod cannot be signed by the given plugin.
 func PodUnsignable() *framework.PodSignatureResult {
 	return &framework.PodSignatureResult{Signable: false}
 }
 
+// The given pod can be signed, and the appropriate signature
+// is just an empty string.
 func EmptyPodSignature() *framework.PodSignatureResult {
 	return &framework.PodSignatureResult{Signable: true}
 }
 
+// Create a signature response from an arbitrary go object.
+// This assumes the object is json serializable.
+// Note that the golang json serializer has fixed ordering for structs and maps, so it is stable:
+//
+//	https://stackoverflow.com/questions/18668652/how-to-produce-json-with-sorted-keys-in-go
+//
+// However, there are more efficient and cleaner ways to serialize which we can
+// consider in the future.
 func PodSignatureFromObj(object any) *framework.PodSignatureResult {
 	marshalled, err := json.Marshal(object)
 	return &framework.PodSignatureResult{Signable: err == nil, Signature: string(marshalled), Error: err}
